e8428660bb6cedf21183f25a060713373b998fd4
Nico <nnobili@fi.uba.ar>
2024-08-31 11:40:40 -0300

    tp1 ej1 update

--- Practicas/Practica1/parte2c/parte2c.ino
+++ Practicas/Practica1/parte2c/parte2c.ino
@@ -30,12 +30,20 @@ void loop()
   float Ton = map(potValue, 0, 1023, 500, 2500);
   float duty=map(Ton, 0, PWMperiod, 0, 1024); // duty=0 is output ALWAYS low, duty=1024 is output ALWAYS high. To produce PWM of X ms, calculate accordingly
   
-  Serial.println(duty);
   Timer1.pwm(PWMoutput,duty);
   ; //do not use delayMicroseconds with values higher than 16000
 
   unsigned long elapsedTime = (micros() - startTime)/1000;
   delay(loop_period - elapsedTime);
+}
+
+
+unsigned int leer_angulo_pote(int pin){
+  // Se lee el valor de tension del potenciometro entre 0V y 5V
+  int potValue = analogRead(pin);
+  // Se tranforma la lectura en un angulo
+  int angulo = map(potValue, 0, 1023, 0, 285);
+  return angle
 }
 
 void timer_callback()
