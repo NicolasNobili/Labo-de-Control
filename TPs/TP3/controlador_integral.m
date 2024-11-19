a4dcc0f384a93ae6ce3046511b2e2ec447b3afc0
cristian milidoni <cristianmilidoni@gmail.com>
2024-11-19 18:57:03 -0300

    qk

--- TPs/TP3/controlador_integral.m
+++ TPs/TP3/controlador_integral.m
@@ -16,17 +16,17 @@ D_monio_r = 0;
 planta_seguimiento_ss_r = ss(A_monio_r,B_monio_r,C_monio_r,D_monio_r);
 planta_seguimiento_ss_r_d = c2d(planta_seguimiento_ss_r,Ts,'zoh');
 
-plc_c = [ -5 - 7.8646i ; -5 + 7.8646i ; -10+2i; -10-2i ; -80 ; -100];
+plc_c = [ -5 - 7.8646i ; -5 + 7.8646i ; -10+2i; -10-2i ; -80];
 
 plc_d = exp(plc_c * 0.01);
-
+%{
 K  = -place(planta_seguimiento_ss_r_d.A , planta_seguimiento_ss_r_d.B(:,1) , plc_d);
 
 % Sistema lazo cerrado:
 A_des = planta_seguimiento_ss_r_d.A + planta_seguimiento_ss_r_d.B(:,1) * K;
 B_des = planta_seguimiento_ss_r_d.B(:,2:3); % Solo la parte de la referencia, con la entrada posta se realimenta.
 C_des = planta_seguimiento_ss_r_d.C;
-
+%}
 % El problema de querer agregar accion integral para ambas salidas es que
 % las matrices A y B del sistema con estado aumentado no forman un par
 % controlable. Algo parecido pasaba al calcular la matriz de feedforward,
@@ -49,7 +49,7 @@ D_monio_r = 0;
 planta_ss_r = ss(A_monio_r,B_monio_r,C_monio_r,D_monio_r);
 planta_ss_r_d = c2d(planta_ss_r,Ts,'zoh');
 
-plc_c = [ -5 - 7.8646i ; -5 + 7.8646i ; -10+2i; -10-2i ; -8];
+plc_c = [-5 - 7.8646i ; -5 + 7.8646i ; -10+2i; -10-2i  ; -7];
 
 plc_d = exp(plc_c * 0.01);
 
